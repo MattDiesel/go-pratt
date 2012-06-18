dbff7db64fcfb011840711148a1f2c5fde300b35
Matt Diesel <M@ttDiesel.co.uk>
2012-06-18 12:37:14 +0100

    Added example of add and sub operators

--- test.go
+++ test.go
@@ -67,22 +67,90 @@ func NewLexer(ch chan parser.IToken, p parser.IParser, input string) {
 	close(ch)
 }
 
+type addOp struct { // implements IToken
+	Parent parser.IParser
+}
+
+func (this *addOp) Name() string {
+	return "+"
+}
+
+func (this *addOp) Lbp() parser.Precedence {
+	return 10
+}
+
+func (this *addOp) Led(left parser.IValue) (parser.IValue, error) {
+	right, err := this.Parent.Expression(10)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ai, _ := left.(*Integer)
+	bi, _ := right.(*Integer)
+
+	return NewInteger(ai.value + bi.value), nil
+}
+
+func (this *addOp) Nud() (parser.IValue, error) {
+	return this.Parent.Expression(30)
+}
+
+func (this *addOp) SetParser(p parser.IParser) {
+	this.Parent = p
+}
+
+func (this *addOp) GetParser() parser.IParser {
+	return this.Parent
+}
+
+type subOp struct { // implements IToken
+	Parent parser.IParser
+}
+
+func (this *subOp) Name() string {
+	return "-"
+}
+
+func (this *subOp) Lbp() parser.Precedence {
+	return 10
+}
+
+func (this *subOp) Led(left parser.IValue) (parser.IValue, error) {
+	right, err := this.Parent.Expression(10)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ai, _ := left.(*Integer)
+	bi, _ := right.(*Integer)
+
+	return NewInteger(ai.value - bi.value), nil
+}
+
+func (this *subOp) Nud() (parser.IValue, error) {
+	right, err := this.Parent.Expression(30)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bi, _ := right.(*Integer)
+
+	return NewInteger(-bi.value), nil
+}
+
+func (this *subOp) SetParser(p parser.IParser) {
+	this.Parent = p
+}
+
+func (this *subOp) GetParser() parser.IParser {
+	return this.Parent
+}
+
 func main() {
 	p := parser.NewParser()
-
-	add := &parser.InfixOperator{"+", 10, func(a, b parser.IValue) (parser.IValue, error) {
-		ai, _ := a.(*Integer)
-		bi, _ := b.(*Integer)
-
-		return NewInteger(ai.value + bi.value), nil
-	}, nil}
-
-	sub := &parser.InfixOperator{"-", 10, func(a, b parser.IValue) (parser.IValue, error) {
-		ai, _ := a.(*Integer)
-		bi, _ := b.(*Integer)
-
-		return NewInteger(ai.value - bi.value), nil
-	}, nil}
 
 	mul := &parser.InfixOperator{"*", 20, func(a, b parser.IValue) (parser.IValue, error) {
 		ai, _ := a.(*Integer)
@@ -100,14 +168,14 @@ func main() {
 
 	end := &parser.Token{"(end)", -1, nil}
 
-	p.Add(add)
-	p.Add(sub)
+	p.Add(&addOp{nil})
+	p.Add(&subOp{nil})
 	p.Add(mul)
 	p.Add(div)
 	p.Add(end)
 
 	ch := make(chan parser.IToken)
-	go NewLexer(ch, p, "5+4*3")
+	go NewLexer(ch, p, "5+4*-3")
 
 	lexer := &MyLexer{parser.Position{1, 0}, ch, p}
 
